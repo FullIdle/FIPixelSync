2e38bcbfb40ca62b6d04913fd1bb3fb97cfb57c5
FullIdle <2675423979@qq.com>
2025-08-23 23:41:28 +0800

    发送的异步用future来执行(发现关服的时候，bukkit会先把定制任务全部取消)

--- src/main/java/org/figsq/fipixelsync/fipixelsync/comm/CommManager.java
+++ src/main/java/org/figsq/fipixelsync/fipixelsync/comm/CommManager.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class CommManager {
     public static final Map<Class<? extends IMessage>, IHandler<? extends IMessage>> registeredMessageHandler = new HashMap<>();
@@ -45,8 +46,7 @@ public class CommManager {
     }
 
     public static void publish(final IMessage message) {
-        val bukkitScheduler = Bukkit.getScheduler();
-        bukkitScheduler.runTaskAsynchronously(Main.INSTANCE, () -> {
+        CompletableFuture.runAsync(() -> {
             try (val resource = ConfigManager.redis.getResource()) {
                 resource.publish(CHANNEL_BYTES, encode(message));
             }
